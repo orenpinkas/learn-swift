558aa60a6586d6f858d24cd9d9db1401aebfaa26
Oren Pinkas <oren@cherrypick-consulting.com>
2025-04-21 10:02:00 +0300

    Dex project setup

---
+++ Dex/Dex/DexApp.swift
@@ -0,0 +1,20 @@
+//
+//  DexApp.swift
+//  Dex
+//
+//  Created by Oren Pinkas on 21/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct DexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
